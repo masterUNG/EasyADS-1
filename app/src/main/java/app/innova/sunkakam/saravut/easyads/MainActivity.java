29dc94172f93bd6a46211df4a51e96cd205b55ae
masternock <nock.nack@gmail.com>
2017-10-10 14:10:07 +0700

    Add Fragment Success

--- app/src/main/java/app/innova/sunkakam/saravut/easyads/MainActivity.java
+++ app/src/main/java/app/innova/sunkakam/saravut/easyads/MainActivity.java
@@ -3,12 +3,23 @@ package app.innova.sunkakam.saravut.easyads;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.innova.sunkakam.saravut.easyads.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager() .beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()) .commit();
+
+        }
+
     } //Main Method
 
 } //Main Class
